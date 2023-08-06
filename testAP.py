a482ff120cd9905efd6aa8e0cf24bdb23d6f5fde
Mustaq Desai <mustaq.desai@gmail.com>
2023-08-05 19:04:07 -0700

    Removed uneeded function.

--- testAP.py
+++ testAP.py
@@ -1,22 +1,28 @@
-import sys
-def testAP():
-    print("Script needs 3 arguments:")
-    print("The firmware version, the friendly name of AP, and band (2G or 5G) being tested.")
-    print("CLI example: python3 testAP.py 1.23.456 UAP-AC-Pro 2G") 
+import sys # Need it read command line arguments
     
-    # confirm all requried input is entered
-    arg_count = len(sys.argv)
-    print(arg_count)
+print("Script needs at least 4 arguments:")
+print("Firmware version, friendly AP name, band (2G or 5G), followed by ip address of client(s)")
+print("CLI example: python3 testAP.py 1.23.456 UAP-AC-Pro 2G 23") 
 
-    if arg_count != 4:
-        print("Provide 3 arguments.")
-    else:
-        # sys.argv[0] is the name of file, so we need next 3
-        firmware = sys.argv[1]
-        ap_name = sys.argv[2]
-        band = sys.argv[3]
-        print(firmware)
-        print(ap_name)
-        print(band)
+# confirm all requried input is entered
+cli_args = sys.argv #store all the commnd line args
+arg_count = len(cli_args)
+print(arg_count)
+print(cli_args)
 
-testAP()
+if arg_count < 5:
+    print("Provide at least 4 arguments.")
+else:
+    # cli_args[0] is the name of file, so we need next 3
+    firmware = cli_args[1]
+    ap_name = cli_args[2]
+    band = cli_args[3]
+    print(firmware)
+    print(ap_name)
+    print(band)
+    
+    clients = cli_args[4:arg_count]
+    print(clients)
+
+    ipPool="10.1.21." # for use as prefix later, in a for-loop
+    # string loop with element #4 (5th element). Prior elements are used above.
