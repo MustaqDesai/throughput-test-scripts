ba8891b8aa26eb791e78871fc0f284fe562778e5
Mustaq Desai <mustaq.desai@gmail.com>
2023-08-05 20:58:02 -0700

    Added code to create folder and log file.

--- testAP.py
+++ testAP.py
@@ -1,10 +1,15 @@
-import sys # Need it to read command line arguments
+import sys # Needed to read command line arguments
+import os # Needed create folders and files
+import datetime # Needed for use date and time
+
+#right_now = datetime.datetime.now().strftime("%04Y-%02m-%02d-%H-%M-%S")
+#print(right_now)
 
 # Modify ip address pool if needed, but should not have to modify it often. 
 ipPool="10.1.21." # for use as prefix later, in a for-loop 
 
 intro_message = """Script needs at least 4 arguments:
-Firmware version, friendly AP name, band (2G or 5G), followed by IP address of client(s).
+Firmware version, friendly AP name, Wi-Fi band (2G or 5G), followed by IP address of client(s).
 For client IP address, enter only the last octet.
 Example: python3 testAP.py 1.23.456 UAP-AC-Pro 2G 23"""  
 #print(intro_message)
@@ -20,7 +25,21 @@ else:
     ap_name = cli_args[2]
     band = cli_args[3]
     
+    # Create new folders if needed to store logs
+    os.mkdir(firmware/ap_name) 
+    # Store today's date for use in log file name
+    date_today = datetime.datetime.now().strftime("%04Y-%02m-%02d")
+    # Contruct log file name
+    log_file = date_today + firmware + ap_name + band + ".log"
+    # Contruct log file location
+    log_location = firmware/ap_name/log_file
+    # Show the location and name of log file
+    print("Logging results in %s.") % (log_location)
+    
     clients = cli_args[4:arg_count] # Create list of mobile clients
-    # Below line works same as above line
+    # Below line works same as above line, but I like how the above line looks
     #clients = cli_args[4:]
     print(clients)
+
+    #for client in clients:
+
