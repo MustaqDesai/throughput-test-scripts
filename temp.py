08ef0dd6797e954c012c7179e1b46cfae1bca64c
Mustaq Desai <mustaq.desai@gmail.com>
2023-08-23 21:22:17 -0700

    Functional code with iPerf3 library.

--- temp.py
+++ temp.py
@@ -1,5 +1,14 @@
 import iperf3
 iperf_client = iperf3.Client()
 iperf_client.server_hostname = "10.1.32.22"
+iperf_client.json_output = False
 result = iperf_client.run()
-print(result)
+#print(result)
+#del iperf_client
+#iperf_client = None
+
+#iperf_client = iperf3.Client()
+#iperf_client.server_hostname = "10.1.32.22"
+#iperf_client.reverse = True
+#result = iperf_client.run()
+#print(result)
--- testAP.py
+++ testAP.py
@@ -1,16 +1,16 @@
 import sys # To read command line arguments
 from pathlib import Path # To create folders and files
 import datetime # To use date and time
-import iperf3 # To create and use iperf3 client
+import iperf3 # To create and use iPerf3 client
 
-#right_now = datetime.datetime.now().strftime("%04Y-%02m-%02d-%H-%M-%S")
-#print(right_now)
+#current_datetime = datetime.datetime.now().strftime("%04Y-%02m-%02d-%H-%M-%S")
+#print(current_datetime)
 
 # Modify ip address pool if needed, but should not have to modify it often. 
-ip_pool = "10.1.32." # for use as prefix later, in the for-loop with iperf servers
+ip_pool = "10.1.32." # for use as prefix later, in a for-loop with iperf servers
 
-duration = 30 # For quick tests
-#duration = 600  # To give controller more than 5 minutes to gather data for test client
+test_duration = 10 # For quick tests
+#duration = 600  # To give UniFi Network application more than 5 minutes to gather data for test client
 
 intro_message = """Script needs at least 4 arguments:
 Firmware version, friendly AP name, Wi-Fi band (2G or 5G), followed by IP address of client(s).
@@ -18,24 +18,24 @@ For client IP address, enter only the last octet.
 Example: python3 testAP.py 1.23.456 UAP-AC-Pro 2G 23"""  
 #print(intro_message)
 
-cli_args = sys.argv # Store the commnd line args
+cli_args = sys.argv # Store commnd line args
 arg_count = len(cli_args)
 
 if arg_count < 5: # Confirm requried input is entered
     print(intro_message)
 else:
-    # Name of this file is cli_args[0], so we need next 3 args
+    # Name of this file is cli_args[0], use next 3 args
     firmware = cli_args[1]
     ap_name = cli_args[2]
     band = cli_args[3]
     
     # Create folders to store logs
     Path("./firmware/ap_name").mkdir(parents=True, exist_ok=True)
-    # Store today's date for use in log file name
-    date_today = datetime.datetime.now().strftime("%04Y-%02m-%02d")
+    # Store current date for use in log file name
+    current_date = datetime.datetime.now().strftime("%04Y-%02m-%02d")
     # Contruct log file name
-    log_file = date_today + "-" + firmware + "-" + ap_name + "-" + band + ".log"
-    # Contruct log file location
+    log_file = current_date + "-" + firmware + "-" + ap_name + "-" + band + ".log"
+    # Contruct log folder and file location
     log_location = firmware + "/" + ap_name + "/" + log_file
     #print(log_location)
     # Show the location and name of log file
@@ -44,36 +44,17 @@ else:
     
     iperf_servers = cli_args[4:arg_count] # Create list of iperf servers
     # Below line works same as above line, but I like how the above line looks
-    #clients = cli_args[4:]
-    #print(clients)
-
-    base_command = "" # Start with empty string, to build an iPerf3 command
+    # clients = cli_args[4:]
     
-    # Create iPerf3 client
-    iperf_client = iperf3.Client()
-    iperf_client.duration = duration
-    iperf_client.verbose =  True
-    #iperf_client.json_output = False
-    #iperf_client = True
-
-
-    # Need to rememer that tests are controlled/executed/run from server, but are executed for mobile clients
-    # DN is when the sever is receving traffic from client
-    # UP is when the server is sending traffic to client
+    # Tests are executed/run from a linux machine (test runner), 
+    # but are executed on iperf server running on test device(s). 
+    # DN is when the test runner is receving traffic from test device
+    # UP is when the test runner is sending traffic to test device
     directions=["DN","UP"]
     # Main loop that sets direction of iPerf3 tests
     for direction in directions:
         print(direction) # Show direction on screen
-        if direction == "DN":
-            # The -R sets the reverse direction, so the mobile client generates and sends the data to server
-            base_command = "iperf3 --forceflush -t" + str(duration) + " -i5 -V -R -c"
-            iperf_client.reverse = True
-            #print(base_command)
-        else:
-            base_command = "iperf3 --forceflush -t" + str(duration) + " -i5 -V -c"
-            iperf_client.reverse = False
-            #print(base_command)
-    
+        
         # Insert some lines for better visibility and readability of log
         # Need to figure out how to insert content into log file
         #echo "_________________________________________________________________" >> $logLocation
@@ -81,25 +62,52 @@ else:
     
         #echo $direction >> $logLocation # insert direction into log
         for iperf_server in iperf_servers:
-            iperf_client.server_hostname = ip_pool + iperf_server
-            #print(client)
-            # Need detailed time stamp for tests
-            right_now = datetime.datetime.now().strftime("%04Y-%02m-%02d-%H-%M-%S")
-            print(right_now)
-            #echo "$dtNow" >> $logLocation
-
+            print(ip_pool + iperf_server) # Show device ip on screen
+            
+            # Detailed time stamp for tests
+            current_datetime = datetime.datetime.now().strftime("%04Y-%02m-%02d-%H-%M-%S")
+            print(current_datetime)
+            
             # Set number of prallel streams
-            for streams in range(1,2): 
-                print(streams)
-                # The complete iPerf3 command 
-                #complete_command = base_command + ip_pool + client + " -P" + str(streams)
+            for streams in range(1,3): 
+                
+                # Create and use iPerf3 client
+                iperf_client = iperf3.Client()
+                iperf_client.duration = test_duration
+                #iperf_client.verbose = True
+                
+                iperf_client.server_hostname = ip_pool + iperf_server # Use full ip address of device
                 iperf_client.num_streams = int(streams)
-                #print(complete_command)
-                #echo "Executing $finalCommand" #show current command on screen
-                #echo $finalCommand >> $logLocation 
-                #$finalCommand >> $logLocation # insert current command into log
+                if direction == "UP":
+                    iperf_client.reverse = True # Device sends data to test runner
+                
+                print(streams)
+                #print(direction)
+                #print(iperf_client.reverse)
                 result = iperf_client.run()
-                print(result)
+                #print(result)
+
+                # Below code is copied from https://github.com/thiezn/iperf3-python/blob/master/examples/client.py
+                if result.error:
+                    print(result.error)
+                else:
+                    print('')
+                    print('Test completed:')
+                    print('  started at         {0}'.format(result.time))
+                    print('  bytes transmitted  {0}'.format(result.sent_bytes))
+                    print('  retransmits        {0}'.format(result.retransmits))
+                    print('  avg cpu load       {0}%\n'.format(result.local_cpu_total))
+
+                    print('Average transmitted data in all sorts of networky formats:')
+                    print('  bits per second      (bps)   {0}'.format(result.sent_bps))
+                    print('  Kilobits per second  (kbps)  {0}'.format(result.sent_kbps))
+                    print('  Megabits per second  (Mbps)  {0}'.format(result.sent_Mbps))
+                    print('  KiloBytes per second (kB/s)  {0}'.format(result.sent_kB_s))
+                    print('  MegaBytes per second (MB/s)  {0}'.format(result.sent_MB_s))
+                
+                # Can not reuse iPerf3 client after a test is completed.
+                # Destroy after each test to avoid "unable to send cookie to server" error for next run
+                iperf_client = None 
 
 
             
