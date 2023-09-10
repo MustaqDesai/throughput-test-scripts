e667823c4722ff1b5f11233fe21d7d2219b22b73
Mustaq Desai <mustaq.desai@gmail.com>
2023-09-10 15:57:29 -0700

    Fixed grammar.

--- testAP.py
+++ testAP.py
@@ -1,16 +1,16 @@
-import sys # To read command line arguments
-from pathlib import Path # To create folders and files
-import datetime # To use date and time
-import iperf3 # To create and use iPerf3 client
+import sys  # To read command line arguments
+from pathlib import Path  # To create folders and files
+import datetime  # To use date and time
+import iperf3  # To create and use iPerf3 client
 
 #current_datetime = datetime.datetime.now().strftime("%04Y-%02m-%02d-%H-%M-%S")
 #print(current_datetime)
 
 # Modify ip address pool if needed, but should not have to modify it often. 
-ip_pool = "10.1.32." # for use as prefix later, in a for-loop with iperf servers
+ip_pool = "10.1.32."  # for use as prefix later, in a for-loop with iPerf3 servers
 
-test_duration = 10 # For quick tests
-#duration = 600  # To give UniFi Network application more than 5 minutes to gather data for test client
+test_duration = 10  # For quick tests
+#test_duration = 600  # To give UniFi Network application more than 5 minutes to gather data for test client
 
 intro_message = """Script needs at least 4 arguments:
 Firmware version, friendly AP name, Wi-Fi band (2G or 5G), followed by IP address of client(s).
@@ -18,29 +18,29 @@ For client IP address, enter only the last octet.
 Example: python3 testAP.py 1.23.456 UAP-AC-Pro 2G 23"""  
 #print(intro_message)
 
-cli_args = sys.argv # Store commnd line args
+cli_args = sys.argv  # Store command line args
 arg_count = len(cli_args)
 
-if arg_count < 5: # Confirm requried input is entered
+if arg_count < 5: # Confirm required input is entered
     print(intro_message)
 else:
     # Name of this file is cli_args[0], use next 3 args
     firmware = cli_args[1]
     ap_name = cli_args[2]
     band = cli_args[3]
-    iperf_servers = cli_args[4:arg_count] # Create list of iperf servers
-    # Below line works same as above line, but I like how the above line looks
+    iperf_servers = cli_args[4:arg_count] # Create list of iPerf3 servers
+    # Below line works same as above line, but I like how above line looks
     # iperf_servers = cli_args[4:]
     
     # Store current date for use in log file name
     current_date = datetime.datetime.now().strftime("%04Y-%02m-%02d")
    
-    # Create folders in parent direcotry to store log files
+    # Create folders in parent directory to store log files
     log_path = "../" + firmware + "/" + ap_name
     Path(log_path).mkdir(parents=True, exist_ok=True)
-     # Contruct log file name
+     # Construct log file name
     log_file_name = current_date + "-" + firmware + "-" + ap_name + "-" + band + ".log"
-    # Contruct log folder and file location
+    # Construct log folder and file location
     log_location = log_path + "/" + log_file_name
     #print(log_location)
     # Show the location and name of log file
@@ -49,19 +49,15 @@ else:
     # Write to log file
     with open(log_location,'a') as out_file:
         # Tests are executed/run from a linux machine (test runner), 
-        # but are executed on iperf server running on test device(s). 
-        # DN is when the test runner is receving traffic from test device
+        # but are executed on iPerf3 server running on test device(s). 
+        # DN is when the test runner is receiving traffic from test device
         # UP is when the test runner is sending traffic to test device
         directions=["DN","UP"]
         # Main loop that sets direction of iPerf3 tests
         for direction in directions:
             print(direction) # Show direction on screen
             out_file.write(direction + "\n")
-            # Insert some lines for better visibility and readability of log
-            # Need to figure out how to insert content into log file
-            #echo "_________________________________________________________________" >> $logLocation
-            #echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" >> $logLocation
-        
+            
             #echo $direction >> $logLocation # insert direction into log
             for iperf_server in iperf_servers:
                 server_address = ip_pool + iperf_server # compose full ip address of device
@@ -75,7 +71,7 @@ else:
                 print(server_address_and_time_stamp)
                 out_file.write(server_address_and_time_stamp + "\n")                           
                 
-                # Set number of prallel streams
+                # Set number of parallel streams
                 for streams in range(1,3): 
                     
                     # Create and use iPerf3 client
@@ -105,7 +101,7 @@ else:
                         #print('  retransmits        {0}'.format(result.retransmits))
                         #print('  avg cpu load       {0}%\n'.format(result.local_cpu_total))
 
-                        #print('Average transmitted data in all sorts of networky formats:')
+                        #print('Average transmitted data in all sorts of network formats:')
                         #print('  bits per second      (bps)   {0}'.format(result.sent_bps))
                         #print('  Kilobits per second  (kbps)  {0}'.format(result.sent_kbps))
                         #print('  Megabits per second  (Mbps)  {0}'.format(result.sent_Mbps))
